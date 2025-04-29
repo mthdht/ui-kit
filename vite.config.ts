37c51095e189feacde1e6faadd174ed7359e6d38
mthdht <mthdht@gmail.com>
2025-04-29 10:09:07 +0200

    install tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,36 @@
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), tailwindcss(),],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/library.js'),
+      name: 'Ui-kit',
+      // the proper extensions will be added
+      fileName: 'ui-kit',
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['vue'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
 })
